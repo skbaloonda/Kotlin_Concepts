3bb5451b350de0ad688b6ed6f76d9ef7d80baec9
Sunil Kumawat <Skbaloonda@gmail.com>
2026-01-20 14:46:58 +0530

    Added flow control package

---
+++ BasicAndroidKotlinConcept/app/src/main/java/com/example/basicandroidkotlinconcept/Flow_Control/Flow_Control_Intro.kt
@@ -0,0 +1,23 @@
+package com.example.basicandroidkotlinconcept.Flow_Control
+
+/* Flow Control is used to control the flow of execution in a program based on certain conditions or criteria.
+ It allows you to make decisions, repeat actions, and manage the overall logic of your code.
+Common flow control structures include conditional statements (if, else if, else), loops (for, while), and switch statements.
+ */
+
+// Types of Flow Control:
+// 1. Conditional Statements: These statements allow you to execute different blocks of code based on certain conditions.
+//    - if Statement
+//    - if-else Statement
+//    - else-if Ladder
+// 2. Loops: Loops allow you to repeat a block of code multiple times until a certain condition is met.
+//    - for Loop
+//    - while Loop
+//    - do-while Loop
+// 3. Control Transfer Statements: These statements allow you to transfer control to another part of the program.
+//    - break Statement
+//    - continue Statement
+//   - return Statement
+// 4. when Expression: This is a more powerful version of the switch statement found in other languages. It allows you to match a value against multiple conditions and execute the corresponding block of code.
+
+
